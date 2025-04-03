0557b3b5e717363cd2b3bfc422b9a58e017a44a1
Simon A. Berger <simberger@gmail.com>
2025-04-03 20:15:45 +0200

    inline manipulate function

--- src/bin/iterative_minimize.rs
+++ src/bin/iterative_minimize.rs
@@ -98,29 +98,30 @@ impl Board {
                         }
                     }
 
-                    board.manipulate(field, num);
+                    {
+                        let this = &mut board;
+                        let num = num;
+                        assert!(num < 9);
+
+                        this.h_free[F2H[field as usize]].bit_reset(num as usize);
+                        this.v_free[F2V[field as usize]].bit_reset(num as usize);
+                        this.b_free[F2B[field as usize]].bit_reset(num as usize);
+
+                        // let f = &mut self.fields[addr.y][addr.x];
+                        let f = &mut this.fields[field as usize];
+                        assert_eq!(*f, Field::Empty);
+                        *f = Field::Set(num as u8);
+                        // Edit {
+                        //     field,
+                        //     num: num as u8,
+                        // }
+                    };
                 }
             }
         }
         board
     }
 
-    pub fn manipulate(&mut self, field: u8, num: u8) {
-        assert!(num < 9);
-
-        self.h_free[F2H[field as usize]].bit_reset(num as usize);
-        self.v_free[F2V[field as usize]].bit_reset(num as usize);
-        self.b_free[F2B[field as usize]].bit_reset(num as usize);
-
-        // let f = &mut self.fields[addr.y][addr.x];
-        let f = &mut self.fields[field as usize];
-        assert_eq!(*f, Field::Empty);
-        *f = Field::Set(num as u8);
-        // Edit {
-        //     field,
-        //     num: num as u8,
-        // }
-    }
     pub fn rollback(&mut self, field: u8, num: u8) {
         self.h_free[F2H[field as usize]].bit_set(num as usize);
         self.v_free[F2V[field as usize]].bit_set(num as usize);
@@ -164,29 +165,32 @@ fn solve(board: &mut Board) -> bool {
         let cur_num = &mut num_stack[stack_ptr];
         let cur_field = &mut field_stack[stack_ptr];
 
-        let (mut min_candidates, field) = if *cur_candidates == u16::MAX {
+        if *cur_candidates == u16::MAX {
             if board.open.is_empty() {
                 board.print();
                 println!("max depth: {}, steps: {}", max_depth, num_steps);
                 return true;
             }
-            let (min_candidates, min_i) = best_candidate(board);
-            let field = board.open.swap_remove(min_i);
-            (min_candidates, field)
+            let min_i;
+            (*cur_candidates, min_i) = best_candidate(board);
+            *cur_field = board.open.swap_remove(min_i);
         } else {
             board.rollback(*cur_field, *cur_num);
-            (*cur_candidates, *cur_field)
         };
-        let test = min_candidates.trailing_zeros() as u8;
+        let test = cur_candidates.trailing_zeros() as u8;
         if test < 9 {
             // test candidate field:
             // 1. knock out lowest bit
             // 2. 'recursion'
-            min_candidates.bit_reset(test as usize);
-            board.manipulate(field, test);
-            *cur_candidates = min_candidates;
+            cur_candidates.bit_reset(test as usize);
+            assert!(test < 9);
+
+            board.h_free[F2H[*cur_field as usize]].bit_reset(test as usize);
+            board.v_free[F2V[*cur_field as usize]].bit_reset(test as usize);
+            board.b_free[F2B[*cur_field as usize]].bit_reset(test as usize);
+            board.fields[*cur_field as usize] = Field::Set(test as u8);
+
             *cur_num = test;
-            *cur_field = field;
             stack_ptr += 1;
             candidates_stack[stack_ptr] = u16::MAX;
             num_stack[stack_ptr] = 0u8;
@@ -194,7 +198,7 @@ fn solve(board: &mut Board) -> bool {
         } else {
             // unsolvable -> return / backtrack
             stack_ptr -= 1;
-            board.open.push(field);
+            board.open.push(*cur_field);
         }
     }
 }
